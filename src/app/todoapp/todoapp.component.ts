ed90a99202f9b507b7e866dab2bc097bd40e93f1
GauravBaviskar--99 <gauravbaviskarpune@gmail.com>
2021-09-24 10:10:33 +0530

    Pushed todo app

--- src/app/todoapp/todoapp.component.ts
+++ src/app/todoapp/todoapp.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoappComponent implements OnInit {
 
+  todoList = ["Angular", "Angular", "Angular"];
+  taskName: string;
+
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
+  addItemIntoList() {
+    this.todoList.push(this.taskName);
+    this.taskName = "";
+  }
+  removeItemFromList(i) {
+    this.todoList.splice(i, 1)
+  }
 }
